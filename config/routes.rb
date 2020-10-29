d5f150102efbae7dd7ec635a13b5baffa11fad9a
Juan Munoz <juanc.munozv@gmail.com>
2020-10-28 21:21:09 -0700

    cleaning routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,21 +7,4 @@ Rails.application.routes.draw do
       delete :destroy
     end
   end
-
-  resources :polls, only: %i[index create] do
-    member do
-      get :show
-      put :update
-      delete :destroy
-    end
-  end
-
-  # only gets and posts
-  # resources :tests, only: %i[index create] do
-  #   member do
-  #     get :show
-  #     post :update
-  #     post :delete
-  #   end
-  # end
 end
